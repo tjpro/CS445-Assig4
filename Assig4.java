15e815ed8d6ada9280fa3b84f9cfc7b123fda913
tjpro <tprotivnak@gmail.com>
2016-03-13 22:12:09 -0400

    initial commit

---
+++ Assig4.java
@@ -0,0 +1,37 @@
+//Tyler Protivnak
+//File Created 3/13/2016
+//University of Pittsburgh - CS445
+//Prof: John Ramirez, T/Th 9:30-10:45
+//TA: Karin Cox, Tues 4-5
+//
+//Assig 4 file
+//This is the main program for the assignment that deals with recursion and backtracking
+
+import java.io.*;
+import java.util.*;
+
+public class Assig4{
+
+	public static void main(String [] args){
+		new Assig4(args[0]);
+	}
+	
+	public Assig4(String fileName){
+		Scanner fReader;
+		
+		try{
+            fReader = new Scanner(new File(fileName));
+		}
+		catch(Exception e){
+            System.out.println(e);
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
